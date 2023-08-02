599b2d68e9bf2cced7c7f92be6221d0b32ad6d78
lakshyasukhralia <lakshyasukhralia97@gmail.com>
2023-08-03 01:36:46 +0530

    Added parcel for state

--- shared/src/commonMain/kotlin/feature/item/domain/model/Item.kt
+++ shared/src/commonMain/kotlin/feature/item/domain/model/Item.kt
@@ -1,8 +1,12 @@
 package feature.item.domain.model
 
+import dev.icerock.moko.parcelize.Parcelable
+import dev.icerock.moko.parcelize.Parcelize
+
+@Parcelize
 data class Item(
     val id: String,
     val name: String,
     val description: String,
     val price: Float
-)
+): Parcelable
